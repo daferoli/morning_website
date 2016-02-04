9ccce1a892fa3cd059029076c3ab31c74ad46bea
David Feroli <dferoli@redhat.com>
2016-02-04 14:47:40 -0500

    changed forecast return to a callback

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var weather = require('./lib/forecast_service');
 
-weather.getTemp();
+weather.getTemp(function(temps){
+  console.log("Current: " + temps.currently +
+    "\nLow: " + temps.low +
+    "\nHigh: " + temps.high);
+});
