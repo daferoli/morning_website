adee6a4636ead0185edebc0156915548fd6f13c5
David Feroli <dferoli@redhat.com>
2016-02-12 11:51:40 -0500

    make the application into a server

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var weather = require('./lib/forecast_service');
 var trains = require('./lib/lightrail_service');
+var express = require('express');
+var fs = require('fs');
 
-weather.getTemp(function(temps){
-  console.log("Current: " + temps.currently +
-    "\nLow: " + temps.low +
-    "\nHigh: " + temps.high);
+var welcome = fs.readFileSync('welcome.html');
+var app = express();
+
+app.get('/temps',function(req,res){
+  weather.getTemp(function(temps){
+    res.send(JSON.stringify(temps));
+  });
 });
 
-trains.getTrainTimes(3,8,function(timesArrray){console.log(timesArrray);});
+app.get('/trains', function(req,res){
+  trains.getTrainTimes(3,8,function(timesArray){
+    res.send(timesArray.toString());
+  });
+});
+
+app.get('/', function(req,res){
+  res.end(welcome);
+});
+
+app.listen(8081, function(){
+  console.log('server is up and running! port 8081');
+});
